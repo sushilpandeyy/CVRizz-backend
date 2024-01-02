67888ffa3a16869da6c5ad374b53df669784965a
SACHINKUMAR1728 <sachinsubh880@gmail.com>
2024-01-02 22:47:10 +0530

    added login api

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
@@ -15,25 +14,55 @@ const dbName = 'CVmaker';
 
 mongoose.connect(`${url}/${dbName}`).then(() => {
     console.log('MongoDB Connected');
-    app.listen(PORT,() => console.log(`Server is running on http://localhost:${PORT}`))
+    app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
 })
-.catch(err => console.log(err));   
+    .catch(err => console.log(err));
 
 const db = mongoose.connection;
 
-app.use(bodyParser.json());
-
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const User = require('./models/User');
 
-app.get('/new', (req, res) => {
-    res.send('Hello World!');
-});
+app.get('/new', async (req, res) => {
+    const data = await User.find({ password: "8052202123" });
+    res.json(data);
+    console.log(data);
+    if (data.password === "8052202123") {
+        console.log("success");
+    }
 
+});
+//update
+app.put('/update', async (req, res) => {
+    console.log(req.body);
+    await User.updateOne({ _id: req.body.id }, { username: "manmohan" });
+    res.send({ success: true, message: "data updated" });
+});
+app.post('/login', async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        console.log(username,email, password);
+        const data = await User.find({ email: email, password: password });
+        if (data.length === 0) {
+            res.status(401).send('Invalid credentials');
+        }else{      
+            console.log(data);
+            res.status(201).json(data);
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+
+
+    }
+});
 app.post('/users', async (req, res) => {
     try {
         const { username, email, password } = req.body;
+        console.log(username, email, password);
         const user = new User({ username, email, password });
         const collection = db.collection('users');
         await collection.insertOne({ username, email, password }); // Use await for async operations
--- public/script.js
+++ public/script.js
@@ -1,4 +1,31 @@
-function createUser() {
+function login() {
+  const username = document.getElementById('username').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  // Create a user object
+  const user = { username, email, password };
+
+  // Send the user data to the server using fetch
+  fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('User login', data);
+    alert('User login successfully!');
+  })
+  .catch(error => {
+    console.error('Error loggin user:', error);
+    alert('Error login user. Please try again.');
+  });
+}
+
+function signup() {
     const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
@@ -11,17 +38,17 @@ function createUser() {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-      },
+      },  
       body: JSON.stringify(user),
-    })
+    })  
     .then(response => response.json())
     .then(data => {
       console.log('User created:', data);
       alert('User created successfully!');
-    })
+    })  
     .catch(error => {
       console.error('Error creating user:', error);
       alert('Error creating user. Please try again.');
-    });
-  }
-  
+    });  
+  }  
+
